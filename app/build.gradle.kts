c42b2b445e2417c6846d8713e5a6e8bacb99be9b
Mustinsar Saeed <mustinsarsaeed027@gmail.com>
2024-09-22 07:54:41 +0500

    SDK version Name 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.itune"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
